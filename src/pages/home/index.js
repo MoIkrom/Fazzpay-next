382778c117f45471fb9b84d321713078d60f05fb
MoIkrom <mikram216@gmail.com>
2023-01-10 01:16:59 +0700

    change Component Footer

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,7 +5,7 @@ import styles from "../../styles/Rumah.module.css";
 
 //import components
 import Header from "../../components/Header/Header";
-import Footer from "../../components/footer/Footer";
+import Footer from "../../components/Footer/Footer";
 import Sidebar from "../../components/sidebar/Sidebar";
 import CardTransaction from "../../components/CardTransaction/index";
 
