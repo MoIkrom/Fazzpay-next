e8559d3c1e9f84f4c625414ea4a8948ca310fc4a
MoIkrom <mikram216@gmail.com>
2023-01-17 14:45:42 +0700

    config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_BACKEND_URL,
-    CLOUDINARY_LINK: process.env.CLOUDINARY_LINK,
-    CLOUD: process.env.CLOUD,
+    NEXT_PUBLIC_BACKEND_URL: "https://fazzpay-rose.vercel.app",
+    CLOUDINARY_LINK: "https://res.cloudinary.com/dd1uwz8eu/image/upload/v1666604839/Fazzpay/example_qx2pf0.png",
+    CLOUD: "https://res.cloudinary.com/dd1uwz8eu/image/upload/v1666604839/",
+    // NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_BACKEND_URL,
+    // CLOUDINARY_LINK: process.env.CLOUDINARY_LINK,
+    // CLOUD: process.env.CLOUD,
   },
   images: {
     domains: ["res.cloudinary.com"],
